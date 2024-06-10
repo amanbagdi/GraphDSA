171fe22c56f4457d5079df41ae4be03a56b0b0b9
amanbagdi <amanbagdi5@gmail.com>
2024-06-10 22:19:45 +0530

    Dijkstras Algorithm for shortest path

--- src/main/java/org/graph/algos/shortestpath/DijkstrasAlgo.java
+++ src/main/java/org/graph/algos/shortestpath/DijkstrasAlgo.java
@@ -1,7 +1,57 @@
 package org.graph.algos.shortestpath;
 
+import org.graph.algos.WeightedEdge;
+import org.graph.algos.shortestpath.utility.DijkstrasGraphCreation;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class DijkstrasAlgo {
 
+    public static void shortestPath(ArrayList<WeightedEdge>[] graph, boolean[] vis, int[] dist) {
+        PriorityQueue<Pair> queue = new PriorityQueue<>();
+        queue.add(new Pair(0,0));
 
+        while (!queue.isEmpty()) {
+            Pair pair = queue.remove();
+            int curr = pair.node;
+                vis[curr] = true;
+                for (WeightedEdge e: graph[curr]) {
+                    if (!vis[e.dest] && (dist[curr] + e.wt) < dist[e.dest]) {
+                        dist[e.dest] = dist[curr] + e.wt;
+                        queue.add(new Pair(e.dest, dist[e.dest]));
+                    }
+                }
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int node;
+        int distance;
+
+        public Pair(int node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Pair pair2) {
+            return this.distance - pair2.distance;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<WeightedEdge>[] graph = DijkstrasGraphCreation.createGraph();
+        boolean[] vis = new boolean[6];
+        int[] dist = new int[6];
+        dist[0] = 0;
+        for (int i = 1; i < 6; i++)
+            dist[i] = Integer.MAX_VALUE;
+        shortestPath(graph, vis, dist);
+
+        for (int i=0; i<6; i++) {
+            System.out.println(i + " -> " + dist[i]);
+        }
+    }
 
 }
--- src/main/java/org/graph/algos/shortestpath/utility/DijkstrasGraphCreation.java
+++ src/main/java/org/graph/algos/shortestpath/utility/DijkstrasGraphCreation.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 
 public class DijkstrasGraphCreation {
 
-    public static void createGraph(ArrayList<WeightedEdge>[] graph) {
+    public static ArrayList<WeightedEdge>[] createGraph() {
+        ArrayList<WeightedEdge>[] graph = new ArrayList[6];
+
         for (int i=0; i<graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
@@ -17,28 +19,14 @@ public class DijkstrasGraphCreation {
         graph[1].add(new WeightedEdge(1,2,1));
         graph[1].add(new WeightedEdge(1, 3,7));
 
-        graph[2].add(new WeightedEdge(2, 3,4));
+        graph[2].add(new WeightedEdge(2, 4,3));
 
         graph[3].add(new WeightedEdge(3, 5, 1));
 
         graph[4].add(new WeightedEdge(4, 3, 2));
         graph[4].add(new WeightedEdge(4, 5, 5));
 
-
-    }
-
-
-    public static void main(String[] args) {
-        int V = 4;  // v -> no. of total vertices in the graph
-
-        ArrayList<WeightedEdge>[] graph = new ArrayList[V];
-        createGraph(graph);
-
-        System.out.println("Neighbours of node - 1 with their weights are :");
-
-        for (WeightedEdge e: graph[1]) {
-            System.out.println(e.dest + " " + e.wt);
-        }
+        return graph;
     }
 
 }
